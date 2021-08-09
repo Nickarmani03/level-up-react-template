2a39acf5831c4466706a678b307ff041d4c1a27e
Nickarmani03 <nickarmani03@gmail.com>
2021-08-09 09:37:19 -0500

    Joining events

--- src/components/events/EventList.js
+++ src/components/events/EventList.js
@@ -6,7 +6,7 @@ import { useHistory } from "react-router-dom"
 export const EventList = () => {
     const history = useHistory()
     const { events, getEvents} = useContext(EventContext)
-    // const {createEvent, joinEvent } = useContext(EventContext)
+    const {createEvent, joinEvent } = useContext(EventContext)
 
     useEffect(() => {
         getEvents()
@@ -16,8 +16,27 @@ export const EventList = () => {
         <article className="events">
             <header className="events__header">
                 <h1>Level-U-Up Game Events</h1>
-                
+                <button className="btn btn-2 btn-sep icon-create"
+                    onClick={() => {
+                        history.push({ pathname: "/events/new" })
+                    }}
+                >Schedule New Event</button>
             </header>
+            {
+                events.map(event => {
+                    // const attending = profile.events.some(evt => evt.id === event.id)
+                    return <section key={event.id} className="registration">
+                        <div className="registration__game">{event.game.title}</div>
+                        <div>{event.description}</div>
+                        <div>
+                            {event.date} @ {event.time}
+                        </div>
+                        <button className="btn btn-2"
+                                onClick={() => joinEvent(event.id)}
+                        >Join</button>
+                    </section>
+                })
+            }
             {
                 events.map(event => {
                     return <section key={event.id} className="registration">
@@ -36,27 +55,11 @@ export const EventList = () => {
                     </section>
                 })
             }
-            <button className="btn btn-2 btn-sep icon-create"
+            {/* <button className="btn btn-2 btn-sep icon-create"
                     onClick={() => {
                         history.push({ pathname: "/events/new" })
                     }}
-                >Schedule New Event</button>
-
-            {/* {
-                events.map(event => {
-                    const attending = profile.events.some(evt => evt.id === event.id)
-                    return <section key={event.id} className="registration">
-                        <div className="registration__game">{event.game.name}</div>
-                        <div>{event.description}</div>
-                        <div>
-                            {event.date} @ {event.time}
-                        </div>
-                        <button className="btn btn-2"
-                                onClick={() => joinEvent(event.id)}
-                        >Join</button>
-                    </section>
-                })
-            } */}
+                >Schedule New Event</button> */}
         </article >
     )
 }
